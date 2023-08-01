93b067f9801077947da2ea66a1196bb2c9ffd73a
Lucas <lucascomamala@gmail.com>
2023-08-01 15:29:22 -0600

    Devise install

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   
   root 'foods#index'
 
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show]
+  resources :recipes do
     resources :recipe_foods, only: [:new, :create, :destroy, :edit, :update]
   end
   resources :foods
-  resources :recipes
 end
