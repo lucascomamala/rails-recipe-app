8b0512ebd66ef37bfa5d4e78b9c744be2000854e
Lucas <lucascomamala@gmail.com>
2023-08-01 22:54:09 -0600

    Creating new foods

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,6 +4,6 @@ class Food < ApplicationRecord
 
   validates :name, presence: true
   validates :measurement_unit, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'only allows letters' }
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
